bea133606f7324c6dac3c8d5ad158f168b4849e6
motii <108280115+motii8128@users.noreply.github.com>
2023-11-20 22:34:29 +0900

    Add files via upload

--- src/bin/twist_reciever.rs
+++ src/bin/twist_reciever.rs
@@ -6,9 +6,9 @@ use safe_drive::{
     msg::common_interfaces::geometry_msgs,
 };
 
-use rust_robo_utils::connector::udp_bridge;
 use ros2_rust_util::get_str_parameter;
-use async_std::{self, channel::unbounded};
+use easy_robot_driver::*;
+use async_std;
 
 #[async_std::main]
 async fn main()->Result<(), DynError>
@@ -22,15 +22,12 @@ async fn main()->Result<(), DynError>
     let publisher = node.create_publisher::<geometry_msgs::msg::Twist>("/cmd_vel", None)?;
 
     let reciever_addr = get_str_parameter(node.get_name(), "sender_addr", "127.0.0.1:8080");
-    let (sig_s, sig_r) = unbounded();
 
     pr_info!(log, "Start {}", node.get_name());
 
-    let reciever_task = async_std::task::spawn(udp_bridge::udp_twist_reciever(reciever_addr, sig_r, publisher));
-    let signal_task = async_std::task::spawn(udp_bridge::get_signal(sig_s));
+    let reciever = async_std::task::spawn(udp_twist_reciever(reciever_addr, publisher));
 
-    reciever_task.await?;
-    signal_task.await?;
+    reciever.await?;
 
     pr_info!(log, "shutdown server");
 
