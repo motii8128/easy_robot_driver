7529deb5c4050b1046f4ff4628575b7e1d4e5e75
motii <108280115+motii8128@users.noreply.github.com>
2023-11-20 18:31:14 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
 use async_std::channel;
-use async_std::net;
+use async_std::net::*;
 
 use safe_drive::{
     error::DynError,
     topic::{publisher::Publisher, subscriber::Subscriber},
+    msg::common_interfaces::{geometry_msgs, std_msgs},
+    logger::Logger,
+    pr_info,
+    pr_error
 };
 
 pub async fn udp_twist_reciever(
-
+    reciever_addr:String,
+    publisher:Publisher<geometry_msgs::msg::Twist>
 )->Result<(), DynError>
 {
-    
+    let log = Logger::new(publisher.get_topic_name());
+    pr_info!(log, "Start twist reciever({})", publisher.get_topic_name());
+
+    let socket = UdpSocket::bind(reciever_addr).await?;
+
+    let mut buf = [0; 2048];
+
+    loop {
+        match socket.recv_from(&mut buf).await {
+            Ok((data, get_addr))=>{
+
+            }
+            Err(e)=>
+            {
+                pr_error!(log, "[{}] :{:?}", publisher.get_topic_name(), e);
+            }
+        }
+    }
 }
