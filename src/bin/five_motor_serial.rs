07ab9c5723a6630c2e80bbf7c39cf7c4261e82ac
motii <108280115+motii8128@users.noreply.github.com>
2023-11-21 10:33:50 +0900

    Add files via upload

---
+++ src/bin/five_motor_serial.rs
@@ -0,0 +1,45 @@
+use safe_drive::{
+    context::Context,
+    error::DynError,
+    pr_info,
+    logger::Logger,
+    msg::common_interfaces::std_msgs,
+};
+
+use async_std;
+use easy_robot_driver;
+use ros2_rust_util::{get_str_parameter, get_i64_parameter};
+
+#[async_std::main]
+async fn main()->Result<(), DynError>
+{
+    let ctx = Context::new()?;
+
+    let node = ctx.create_node("five_motor_serial", None, Default::default())?;
+
+    let subscriber_fl = node.create_subscriber::<std_msgs::msg::Float32>("/motor/fl", None)?;
+    let subscriber_fr = node.create_subscriber::<std_msgs::msg::Float32>("/motor/fr", None)?;
+    let subscriber_rl = node.create_subscriber::<std_msgs::msg::Float32>("/motor/rl", None)?;
+    let subscriber_rr = node.create_subscriber::<std_msgs::msg::Float32>("/motor/rr", None)?;
+    let subscriber_ex = node.create_subscriber::<std_msgs::msg::Float32>("/motor/ex", None)?;
+
+    let port_name = get_str_parameter(node.get_name(), "port_name", "dev/ttyACM0");
+    let baud_rate = get_i64_parameter(node.get_name(), "baudrate", 115200) as u32;
+
+    let log = Logger::new(node.get_name());
+    pr_info!(log, "Start {}", node.get_name());
+
+    let writer = async_std::task::spawn(easy_robot_driver::five_motor_serial_writer(
+        subscriber_fl, 
+        subscriber_fr, 
+        subscriber_rl, 
+        subscriber_rr, 
+        subscriber_ex, 
+        port_name, 
+        baud_rate));
+
+    writer.await?;
+
+    pr_info!(log, "Shutdown {}", node.get_name());    
+    Ok(())
+}
