cfd979e2c2f90937c5d40e2ab2f82422174f0ac5
motii <108280115+motii8128@users.noreply.github.com>
2023-12-06 11:05:05 +0900

    Update motor_control_test.ino

--- arduino/motor_control_test/motor_control_test.ino
+++ arduino/motor_control_test/motor_control_test.ino
@@ -2,28 +2,25 @@
 
 const int MAX_PWM = 255;
 
-const int PWM_0_PIN = A0;
-const int PWM_1_PIN = A1;
-const int DIGITAL_0_PIN = 0;
-const int DIGITAL_1_PIN = 1;
+const int PWM_PIN = A0;
+const int DIGITAL_PIN = 0;
 
 
-float motor_0, motor_1;
+float motor_0;
 
-void recieve_serial(float *value_a, float *value_b)
+void recieve_serial(float *value)
 {
   if(Serial.available() > 0)
   {
     String get_data = Serial.readStringUntil('\n');
-    int comma = get_data.indexOf(',');
+    int start = get_data.indexOf('s');
+    int end P get_data.indexOf('e');
 
-    if(comma != -1)
+    if(start != -1 && end != -1)
     {
-      String motor_0_str = get_data.substring(0, comma);
-      String motor_1_str = get_data.substring(comma+1);
+      String motor_str = get_data.substring(start, end);
 
-      *value_a = motor_0_str.toFloat();
-      *value_b = motor_1_str.toFloat();
+      *value = motor_str.toFloat();
     }
   }
 }
@@ -46,18 +43,14 @@ void set_motor(int pwm_pin, int digital_pin, float get_value)
 void setup() {
   // put your setup code here, to run once:
   motor_0 = 0.0;
-  motor_1 = 0.0;
 
   pinMode(PWM_0_PIN, OUTPUT);
-  pinMode(PWM_1_PIN, OUTPUT);
   pinMode(DIGITAL_0_PIN, OUTPUT);
-  pinMode(DIGITAL_1_PIN, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  recieve_serial(&motor_0, &motor_1);
+  recieve_serial(&motor_0);
 
   set_motor(PWM_0_PIN, DIGITAL_0_PIN, motor_0);
-  set_motor(PWM_1_PIN, DIGITAL_1_PIN, motor_1);
 }
