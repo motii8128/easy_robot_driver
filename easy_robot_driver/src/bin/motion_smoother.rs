744abcac006ffba4e734dbae3580bf619da07ded
motii <108280115+motii8128@users.noreply.github.com>
2023-12-08 08:37:00 +0900

    Update and rename async_motion_smoother.rs to motion_smoother.rs

---
+++ easy_robot_driver/src/bin/motion_smoother.rs
@@ -0,0 +1,48 @@
+use safe_drive::{
+    context::Context,
+    error::DynError,
+    pr_info,
+    logger::Logger,
+    msg::common_interfaces::std_msgs,
+};
+
+use ros2_rust_util::get_f64_parameter;
+
+async fn main()->Result<(), DynError>
+{
+    let ctx = Context::new()?;
+    let node = ctx.create_node("async_motion_smoother", None, Default::default())?;
+    let mut selector = ctx.create_selector()?;
+    
+    let subscriber = node.create_subscriber::<std_msgs::msg::Float32>("/input", None)?;
+    let publisher = node.create_publisher::<std_msgs::msg::Float32>("/output", None)?;
+
+    let smooth_gain = get_f64_parameter(node.get_name(), "gain", 0.1) as f32;
+    let log = Logger::new(node.get_name());
+
+    let mut target = 0.0;
+    let mut history = 0.0;
+
+    selector.add_subscriber(
+        subscriber,
+        Box::new(move |msg|{
+            target = msg.data;
+            let mut send_msg = std_msgs::msg::Float32::new().unwrap();
+
+            let vec = target - history;
+            if vec > smooth_gain
+            {
+                if target > history
+                {
+                    send_msg.data = history + smooth_gain
+                }
+            }
+        })
+    );
+    
+    pr_info!(log, "Start {}", node.get_name());
+
+    loop{
+        selector.wait()?;
+    }
+}
